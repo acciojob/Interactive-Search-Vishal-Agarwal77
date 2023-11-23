362c1aace169e0e1fa9cb3cc6d6f63b321735b1f
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-23 18:57:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //your JS code here. If required.
 const searchbox=document.getElementsByClassName("search")[0];
+const searchinput=document.getElementsByClassName("input")[0];
 console.log(searchbox);
 searchbox.addEventListener("click",()=>{
-	searchbox.classList.add("active");
+	searchbox.classList.toggle("active");
+	if(searchbox.classList.contains("active")){
+		searchinput.focus();
+	}
 })
+// searchbox.classList.remove("active");
