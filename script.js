078996ee2a4f63b27239e4f979b64875c5b7df6c
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-23 18:47:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const searchbox=document.getElementsByClassName("search")[0];
+console.log(searchbox);
+searchbox.addEventListener("click",()=>{
+	searchbox.classList.add("active");
+})
